8fe94bb00f4285e32d97e7ae5f051816c9b42d60
Idriss Neumann <idriss.neumann@comwork.io>
2026-07-17 08:54:07 +0100

    feat(cicd): push version tag

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -12,5 +12,6 @@ docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_
 docker buildx bake -f docker-compose-build.yml --push ${SERVICE_NAME}
 docker tag "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION}" "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION_SHA}"
 docker tag "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION}" "${CI_REGISTRY}/${IMAGE_NAME}:latest"
+docker push "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION}"
 docker push "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION_SHA}"
 docker push "${CI_REGISTRY}/${IMAGE_NAME}:latest"
